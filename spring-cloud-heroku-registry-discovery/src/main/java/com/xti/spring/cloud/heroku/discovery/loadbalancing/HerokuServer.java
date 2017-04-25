9a39cb1d481be371045026beeb0e03b250bf6bbb
Michael Willemse <michael.willemse@xt-i.com>
2017-04-26 00:25:43 +0200

    overridden HerokuServer.java superclass equals and hashcode

--- spring-cloud-heroku-registry-discovery/src/main/java/com/xti/spring/cloud/heroku/discovery/loadbalancing/HerokuServer.java
+++ spring-cloud-heroku-registry-discovery/src/main/java/com/xti/spring/cloud/heroku/discovery/loadbalancing/HerokuServer.java
@@ -37,4 +37,23 @@ public class HerokuServer extends Server {
     public MetaInfo getMetaInfo() {
         return metaInfo;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        HerokuServer that = (HerokuServer) o;
+
+        return !(metaInfo != null ? !metaInfo.equals(that.metaInfo) : that.metaInfo != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (metaInfo != null ? metaInfo.hashCode() : 0);
+        return result;
+    }
 }
